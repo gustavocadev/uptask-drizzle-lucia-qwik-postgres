c64007ba5e02b49e40bc9e6dcb3523d2cc0f79c4
gustavocadev <churaalaygustavo11@gmail.com>
2023-03-18 18:42:21 -0500

    feat: use loader

--- src/routes/(authed)/layout.tsx
+++ src/routes/(authed)/layout.tsx
@@ -1,17 +1,29 @@
 import { component$, Slot } from '@builder.io/qwik'
+import { routeLoader$ } from '@builder.io/qwik-city'
 import { Header } from '~/components/ui/Header'
 import { Sidebar } from '~/components/ui/Sidebar'
-import { useAuthSession } from '../plugin@auth'
+import { getUserSession } from '~/utils/session'
+
+export const useLoaderUserData = routeLoader$(async ({ request, env, redirect }) => {
+	const user = await getUserSession(request, env.get('QWIK_AUTH_SECRET') ?? '')
+
+	if (!user) {
+		throw redirect(303, '/')
+	}
+
+	return {
+		name: user.name,
+	}
+})
 
 export default component$(() => {
-	const userSignal = useAuthSession()
+	const userData = useLoaderUserData()
 	return (
 		<>
 			<section>
-				<Header loggedIn={userSignal.value?.user !== undefined} />
-				<pre>{JSON.stringify(userSignal.value, null, 2)}</pre>
+				<Header />
 				<div class="md:flex md:min-h-screen">
-					<Sidebar />
+					<Sidebar userData={userData.value} />
 					<main class="flex-1 p-10">
 						<Slot />
 					</main>
