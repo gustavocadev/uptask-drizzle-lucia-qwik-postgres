1d3ec3495a2089a7fb36e5fbefe9d4a5f687b9da
gustavocadev <churaalaygustavo11@gmail.com>
2023-03-19 18:46:03 -0500

    feat: add media queries

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -5,20 +5,24 @@ import { useAuthSignout } from '~/routes/plugin@auth'
 export const Header = component$(() => {
 	const actionSignout = useAuthSignout()
 	return (
-		<header class="px-4  py-5 bg-white border-b">
+		<header class="px-4 py-5 bg-white border-b">
 			<nav class="md:flex md:justify-between">
-				<h2 class="text-4xl text-sky-600 font-black text-center">Uptask</h2>
-				<input
-					type="search"
-					placeholder="Buscar proyecto"
-					class="rounded-lg lg:w-96 block p-2 border"
-				/>
+				<h2 class="text-4xl text-sky-600 font-black text-center mb-5 md:mb-0">
+					<Link href="/projects">Uptasks</Link>
+				</h2>
 
-				<div class="flex items-center gap-4">
+				<div class="flex flex-col md:flex-row items-center gap-4">
+					<Form spaReset>
+						<input
+							type="input"
+							class="border border-gray-300 p-3 rounded-md"
+							placeholder="Buscar"
+							name="search"
+						/>
+					</Form>
 					<Link href="/projects" class="font-bold uppercase">
 						Proyectos
 					</Link>
-
 					<Form action={actionSignout}>
 						<button
 							type="submit"
@@ -28,6 +32,7 @@ export const Header = component$(() => {
 							Cerrar sesion
 						</button>
 					</Form>
+					{/* <Busqueda /> */}
 				</div>
 			</nav>
 		</header>
