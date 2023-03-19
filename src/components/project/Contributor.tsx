24c61fde24d435e352b40da04b1cf4e0b7a8d18d
gustavocadev <churaalaygustavo11@gmail.com>
2023-03-19 18:40:56 -0500

    feat: add contributor comp

---
+++ src/components/project/Contributor.tsx
@@ -0,0 +1,64 @@
+import { component$ } from '@builder.io/qwik'
+import type { User } from '@prisma/client'
+import { Form, globalAction$, z, zod$ } from '@builder.io/qwik-city'
+import { prisma } from '~/server/prisma'
+
+export const useActionRemoveContributor = globalAction$(
+	async (values) => {
+		// delete one contributor from the project contributors
+		await prisma.project.update({
+			where: {
+				id: values.projectId,
+			},
+			data: {
+				contributors: {
+					// we use the disconnect method to delete the contributor only from the model of the project
+					disconnect: {
+						id: values.contributorId,
+					},
+				},
+			},
+		})
+
+		return {
+			success: true,
+		}
+	},
+	zod$({
+		contributorId: z.string(),
+		projectId: z.string(),
+	})
+)
+
+type Props = {
+	contributor: User
+	projectId: string
+	authorId: string
+	userAuthId: string
+}
+
+export default component$(({ contributor, projectId, authorId, userAuthId }: Props) => {
+	const actionRemoveContributor = useActionRemoveContributor()
+	return (
+		<div class="border-b p-5 flex justify-between items-center bg-white rounded">
+			<div>
+				<p>{contributor.name}</p>
+				<p class="text-sm text-gray-700">{contributor.email}</p>
+			</div>
+
+			{authorId === userAuthId && (
+				<Form action={actionRemoveContributor}>
+					<input type="hidden" name="projectId" value={projectId} />
+
+					<input type="hidden" name="contributorId" value={contributor.id} />
+
+					<button
+						type="submit"
+						class="bg-red-600 px-4 py-3 text-white uppercase font-bold text-sm rounded-lg">
+						Eliminar
+					</button>
+				</Form>
+			)}
+		</div>
+	)
+})
