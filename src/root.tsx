2e7a20d523573024194518aae9f53997e89eb955
gustavocadev <churaalaygustavo11@gmail.com>
2023-03-24 11:49:58 -0500

    chore: comment context

--- src/root.tsx
+++ src/root.tsx
@@ -1,28 +1,46 @@
-import { component$ } from '@builder.io/qwik'
-import { QwikCityProvider, RouterOutlet, ServiceWorkerRegister } from '@builder.io/qwik-city'
-import { RouterHead } from './components/router-head/router-head'
+import { component$ } from '@builder.io/qwik';
+import {
+  QwikCityProvider,
+  RouterOutlet,
+  ServiceWorkerRegister,
+} from '@builder.io/qwik-city';
+import { RouterHead } from './components/router-head/router-head';
 
-import './global.css'
+import './global.css';
+
+// type SocketClient = {
+// 	socket: Socket | null
+// }
+
+// export const SocketContext = createContextId<SocketClient>('SocketContext')
 
 export default component$(() => {
-	/**
-	 * The root of a QwikCity site always start with the <QwikCityProvider> component,
-	 * immediately followed by the document's <head> and <body>.
-	 *
-	 * Dont remove the `<head>` and `<body>` elements.
-	 */
+  /**
+   * The root of a QwikCity site always start with the <QwikCityProvider> component,
+   * immediately followed by the document's <head> and <body>.
+   *
+   * Dont remove the `<head>` and `<body>` elements.
+   */
 
-	return (
-		<QwikCityProvider>
-			<head>
-				<meta charSet="utf-8" />
-				<link rel="manifest" href="/manifest.json" />
-				<RouterHead />
-			</head>
-			<body lang="en" class="bg-gray-100">
-				<RouterOutlet />
-				<ServiceWorkerRegister />
-			</body>
-		</QwikCityProvider>
-	)
-})
+  // my global state
+  // const socketState = useStore<SocketClient>({
+  // 	socket: null,
+  // })
+
+  // my providers
+  // useContextProvider(SocketContext, socketState)
+
+  return (
+    <QwikCityProvider>
+      <head>
+        <meta charSet="utf-8" />
+        <link rel="manifest" href="/manifest.json" />
+        <RouterHead />
+      </head>
+      <body lang="en" class="bg-gray-100">
+        <RouterOutlet />
+        <ServiceWorkerRegister />
+      </body>
+    </QwikCityProvider>
+  );
+});
