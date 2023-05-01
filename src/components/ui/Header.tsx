f0e5fa72885e72c78e27580eafd169f4d1b55499
gustavocadev <churaalaygustavo11@gmail.com>
2023-04-30 22:18:53 -0500

    feat: create an action to signout

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -1,40 +1,54 @@
-import { component$ } from '@builder.io/qwik'
-import { Form, Link } from '@builder.io/qwik-city'
-import { useAuthSignout } from '~/routes/plugin@auth'
+import { component$ } from '@builder.io/qwik';
+import { Form, Link, globalAction$ } from '@builder.io/qwik-city';
+import { auth } from '~/lib/lucia';
+
+export const useSignoutAction = globalAction$(async (values, event) => {
+  const authRequest = auth.handleRequest(event);
+  const session = await authRequest.validate();
+
+  if (!session) throw event.redirect(303, '/');
+
+  auth.invalidateSession(session.sessionId);
+  authRequest.setSession(null);
+
+  throw event.redirect(303, '/');
+});
 
 export const Header = component$(() => {
-	const actionSignout = useAuthSignout()
-	return (
-		<header class="px-4 py-5 bg-white border-b">
-			<nav class="md:flex md:justify-between">
-				<h2 class="text-4xl text-sky-600 font-black text-center mb-5 md:mb-0">
-					<Link href="/projects">Uptasks</Link>
-				</h2>
+  const signoutAction = useSignoutAction();
+  return (
+    <header class="px-4 py-5 bg-white border-b">
+      <nav class="md:flex md:justify-between">
+        <h2 class="text-4xl text-sky-600 font-black text-center mb-5 md:mb-0">
+          <Link href="/projects">Uptasks</Link>
+        </h2>
 
-				<div class="flex flex-col md:flex-row items-center gap-4">
-					<Form spaReset>
-						<input
-							type="input"
-							class="border border-gray-300 p-3 rounded-md"
-							placeholder="Buscar"
-							name="search"
-						/>
-					</Form>
-					<Link href="/projects" class="font-bold uppercase">
-						Proyectos
-					</Link>
-					<Form action={actionSignout}>
-						<button
-							type="submit"
-							class="
+        <div class="flex flex-col md:flex-row items-center gap-4">
+          <Form spaReset>
+            <input
+              type="input"
+              class="border border-gray-300 p-3 rounded-md"
+              placeholder="Buscar"
+              name="search"
+            />
+          </Form>
+          <Link href="/projects" class="font-bold uppercase">
+            Proyectos
+          </Link>
+          <Form action={signoutAction}>
+            <input type="hidden" name="callbackUrl" value="/signedout" />
+            <button
+              type="submit"
+              class="
           text-white text-sm bg-sky-600 p-3 rounded-md uppercase font-bold
-          ">
-							Cerrar sesion
-						</button>
-					</Form>
-					{/* <Busqueda /> */}
-				</div>
-			</nav>
-		</header>
-	)
-})
+          "
+            >
+              Cerrar sesion
+            </button>
+          </Form>
+          {/* <Busqueda /> */}
+        </div>
+      </nav>
+    </header>
+  );
+});
