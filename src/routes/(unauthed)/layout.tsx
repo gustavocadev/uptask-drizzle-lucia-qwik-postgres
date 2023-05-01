0e6b9613c3ea3d2ccad8a1278190ee4e055af2ce
gustavocadev <churaalaygustavo11@gmail.com>
2023-04-30 22:16:34 -0500

    chore: import auth object from lucia fn

--- src/routes/(unauthed)/layout.tsx
+++ src/routes/(unauthed)/layout.tsx
@@ -1,23 +1,25 @@
-import { component$, Slot } from '@builder.io/qwik'
-import { routeLoader$ } from '@builder.io/qwik-city'
-import { getUserData } from '../../utils/session'
+import { component$, Slot } from '@builder.io/qwik';
+import { routeLoader$ } from '@builder.io/qwik-city';
+import { auth } from '~/lib/lucia';
 
-export const useLoaderData = routeLoader$(async ({ request, env, redirect }) => {
-	const session = await getUserData(request, env)
-	// if (!session) {
-	// 	console.log('There is no session')
-	// 	throw redirect(303, '/api/auth/signin')
-	// }
-	// console.log('There is a session')
-	if (session) throw redirect(303, '/projects')
-})
+export const useLoaderData = routeLoader$(async (event) => {
+  const authRequest = auth.handleRequest(event);
+  const session = await authRequest.validate();
+
+  console.log(
+    'If there is a session redirect to /projects if not redirect to /'
+  );
+  if (session) throw event.redirect(303, '/projects');
+
+  return {};
+});
 
 export default component$(() => {
-	return (
-		<main class="container mx-auto mt-5 md:mt-20 p-5 md:flex md:justify-center ">
-			<section class="md:w-2/3 lg:w-2/5">
-				<Slot />
-			</section>
-		</main>
-	)
-})
+  return (
+    <main class="container mx-auto mt-5 md:mt-20 p-5 md:flex md:justify-center ">
+      <section class="md:w-2/3 lg:w-2/5">
+        <Slot />
+      </section>
+    </main>
+  );
+});
