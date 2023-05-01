058a7bc6b9b9307a9f7d6c0c4fede889aeb1f61e
gustavocadev <churaalaygustavo11@gmail.com>
2023-04-30 22:20:10 -0500

    chore: load user data through lucia functions

--- src/routes/(authed)/layout.tsx
+++ src/routes/(authed)/layout.tsx
@@ -1,34 +1,35 @@
-import { component$, Slot } from '@builder.io/qwik'
-import { routeLoader$ } from '@builder.io/qwik-city'
-import { Header } from '~/components/ui/Header'
-import { Sidebar } from '~/components/ui/Sidebar'
-import { getUserData } from '~/utils/session'
+import { component$, Slot } from '@builder.io/qwik';
+import { routeLoader$ } from '@builder.io/qwik-city';
+import { Header } from '~/components/ui/Header';
+import { Sidebar } from '~/components/ui/Sidebar';
+import { auth } from '~/lib/lucia';
 
-export const useLoaderUserData = routeLoader$(async ({ request, env, redirect }) => {
-	const user = await getUserData(request, env)
+export const useUserDataLoader = routeLoader$(async (event) => {
+  const authRequest = auth.handleRequest(event);
+  const { user } = await authRequest.validateUser();
 
-	if (!user) {
-		throw redirect(303, '/')
-	}
+  if (!user) {
+    throw event.redirect(303, '/');
+  }
 
-	return {
-		name: user.name,
-	}
-})
+  return {
+    name: user.name,
+  };
+});
 
 export default component$(() => {
-	const userData = useLoaderUserData()
-	return (
-		<>
-			<section>
-				<Header />
-				<div class="md:flex md:min-h-screen">
-					<Sidebar userData={userData.value} />
-					<main class="flex-1 p-10">
-						<Slot />
-					</main>
-				</div>
-			</section>
-		</>
-	)
-})
+  const userData = useUserDataLoader();
+  return (
+    <>
+      <section>
+        <Header />
+        <div class="md:flex md:min-h-screen">
+          <Sidebar userData={userData.value} />
+          <main class="flex-1 p-10">
+            <Slot />
+          </main>
+        </div>
+      </section>
+    </>
+  );
+});
