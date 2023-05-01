f57fcfad742e503ee807bb90f219acd2e0858071
gustavocadev <churaalaygustavo11@gmail.com>
2023-05-01 11:16:02 -0500

    chore: import and  invocate the fn in the component

--- src/components/ui/Sidebar.tsx
+++ src/components/ui/Sidebar.tsx
@@ -1,22 +1,21 @@
-import { component$ } from '@builder.io/qwik'
-import { Link } from '@builder.io/qwik-city'
+import { component$ } from '@builder.io/qwik';
+import { Link } from '@builder.io/qwik-city';
+import { useUserDataLoader } from '~/routes/(authed)/layout';
 
-type Props = {
-	userData: {
-		name: string
-	}
-}
+type Props = {};
 
-export const Sidebar = component$((props: Props) => {
-	return (
-		<aside class="md:w-80 lg:w-96 px-5 py-10">
-			<p class="text-xl font-bold">Hola: {props.userData.name}</p>
+export const Sidebar = component$<Props>(() => {
+  const userDataLoader = useUserDataLoader();
+  return (
+    <aside class="md:w-80 lg:w-96 px-5 py-10">
+      <p class="text-xl font-bold">Hola: {userDataLoader.value.name}</p>
 
-			<Link
-				href="/projects/new-project"
-				class="bg-sky-600 w-full text-white uppercase p-3 font-bold block mt-5 text-center rounded-lg">
-				Nuevo proyecto
-			</Link>
-		</aside>
-	)
-})
+      <Link
+        href="/projects/new-project"
+        class="bg-sky-600 w-full text-white uppercase p-3 font-bold block mt-5 text-center rounded-lg"
+      >
+        Nuevo proyecto
+      </Link>
+    </aside>
+  );
+});
