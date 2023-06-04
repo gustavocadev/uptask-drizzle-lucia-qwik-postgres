1fe38a852c3d550ebdc38ad4a83cb89764f9ab53
CA Gustavo <churaalaygustavo11@gmail.com>
2023-06-04 10:38:42 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,11 @@ export default defineConfig(() => {
       headers: {
         'Cache-Control': 'public, max-age=600',
       },
+      resolve: {
+       alias: {
+          ".prisma/client/index-browser": "./node_modules/.prisma/client/index-browser.js"
+        },
+      },
     }
   };
 });
