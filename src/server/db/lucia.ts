b926486c2efd63e337d2f30909498edc2a75f67f
gustavocadev <gustavocadev@gmail.com>
2024-06-25 11:04:43 -0500

    chore: Add Lucia database integration and configuration

---
+++ src/server/db/lucia.ts
@@ -0,0 +1,35 @@
+// src/server/lucia.ts
+import { Lucia } from 'lucia';
+import { DrizzlePostgreSQLAdapter } from '@lucia-auth/adapter-drizzle';
+import { qwikLuciaConfig } from 'qwik-lucia';
+import { db } from './db';
+import { type SelectUser, sessionTable, userTable } from './schema';
+
+const adapter = new DrizzlePostgreSQLAdapter(db, sessionTable, userTable);
+
+export const lucia = new Lucia(adapter, {
+  sessionCookie: {
+    attributes: {
+      // set to `true` when using HTTPS
+      secure: process.env.NODE_ENV === 'production',
+    },
+  },
+  getUserAttributes: (attributes) => {
+    return {
+      email: attributes.email,
+    };
+  },
+});
+
+/*
+IMPORTANT!
+Here we need to use `qwikLuciaConfig` to correctly configure the `handleRequest` function
+*/
+export const { handleRequest } = qwikLuciaConfig(lucia);
+
+declare module 'lucia' {
+  interface Register {
+    Lucia: typeof lucia;
+    DatabaseUserAttributes: Omit<SelectUser, 'id'>;
+  }
+}
