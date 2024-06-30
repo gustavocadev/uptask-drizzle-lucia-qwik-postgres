fabc4572be8aef557fe39258f187942bdb86c41b
gustavocadev <gustavocadev@gmail.com>
2024-06-30 16:21:00 -0500

    use import env

--- src/server/db/lucia.ts
+++ src/server/db/lucia.ts
@@ -11,7 +11,7 @@ export const lucia = new Lucia(adapter, {
   sessionCookie: {
     attributes: {
       // set to `true` when using HTTPS
-      secure: process.env.NODE_ENV === 'production',
+      secure: import.meta.env.PROD,
     },
   },
   getUserAttributes: (attributes) => {
