0c61554b686bb3876adfaa0955c713937df05246
gustavocadev <gustavocadev@gmail.com>
2024-07-02 10:33:41 -0500

    Add functions

--- src/context/task/TaskProvider.tsx
+++ src/context/task/TaskProvider.tsx
@@ -12,6 +12,8 @@ import { TaskContext } from './TaskContext';
 import { SocketContext } from '../socket/SocketContext';
 import type { Task } from '~/server/services/task/entities/task';
 import { useLocation } from '@builder.io/qwik-city';
+import type { UpdateTaskState } from './types/update-task-state';
+import type { UpdateTask } from './types/update-task';
 
 // we need to create a task provider to get the tasks from the server (websocket) otherwise we will not have any tasks to display when using the <Link /> component
 export const TaskProvider = component$(() => {
@@ -48,6 +50,49 @@ export const TaskProvider = component$(() => {
     );
   });
 
-  useContextProvider(TaskContext, useStore({ tasks, getTasksByProjectId }));
+  const deleteTaskById = $((taskId: string, projectId: string): void => {
+    // I need to send the taskId to the server to delete the task
+    socket.value?.send(
+      JSON.stringify({
+        type: 'delete-task',
+        payload: { taskId, projectId },
+      })
+    );
+  });
+
+  const updateTaskState = $(
+    ({ taskId, taskState, userId, projectId }: UpdateTaskState): void => {
+      socket?.value?.send(
+        JSON.stringify({
+          type: 'update-task-state',
+          payload: { taskId, taskState, userId, projectId },
+        })
+      );
+    }
+  );
+
+  const updateTask = $(({ newTask, projectId, taskId }: UpdateTask): void => {
+    socket.value?.send(
+      JSON.stringify({
+        type: 'update-task',
+        payload: {
+          taskId,
+          newTask,
+          projectId,
+        },
+      })
+    );
+  });
+
+  useContextProvider(
+    TaskContext,
+    useStore({
+      tasks,
+      getTasksByProjectId,
+      deleteTaskById,
+      updateTaskState,
+      updateTask,
+    })
+  );
   return <Slot />;
 });
